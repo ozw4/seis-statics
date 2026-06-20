06b4b0ccf28b882109909e10107133e9d8dcca92
ozw4 <zerowarurei@gmail.com>
2026-06-20 01:04:00 +0000

    fix:

---
+++ .issue_forge/project.sh
@@ -0,0 +1,12 @@
+# issue_forge consumer config.
+# Defaults are supplied by vendor/issue_forge.
+
+CODEX_FLOW_PROFILE_WRITE_SANDBOX='danger-full-access'
+CODEX_FLOW_PROFILE_WRITE_REASONING='high'
+CODEX_FLOW_PROFILE_READ_SANDBOX='danger-full-access'
+CODEX_FLOW_PROFILE_READ_REASONING='medium'
+
+CODEX_FLOW_IMPLEMENTATION_REASONING='high'
+CODEX_FLOW_CHECK_FIX_REASONING='high'
+CODEX_FLOW_REVIEW_REASONING='medium'
+CODEX_FLOW_REVIEW_FIX_REASONING='high'
